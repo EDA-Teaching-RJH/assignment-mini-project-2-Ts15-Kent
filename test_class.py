b572c609bd3462d81742b989eba6b14a884a88f8
Ts15-Kent <Ts15@kent.ac.uk>
2026-08-16 23:12:36 +0000

    Established employee class and needed information

---
+++ test_class.py
@@ -0,0 +1,7 @@
+class Employee:
+    # this is the main class that stores the contact information of every employee.
+    def __init__(self, name, age, email, phone):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.phone = phone
